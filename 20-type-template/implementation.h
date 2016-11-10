b68ecbac4ccc10e15e96f33220e65e33eb91a361
Przemyslaw Ciesielski <przemyslaw.ciesielski.ext@nokia.com>
2016-11-10 11:51:52 +0100

    20_tests_run

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstddef>
 //#include <utility>
+#include <boost/optional.hpp>
 
 template<typename T>
 class container_wrapper
@@ -15,9 +16,28 @@ class container_wrapper
 	container_wrapper & operator = (container_wrapper&&) = default;
   std::size_t size() const
 	{ 
-		return _value.size();
+		return _value?1:0;
 	};
 	//container_wrapper<std::vector<T>>(){si=s;};
 	T _value;
+};
+
+template<typename T>
+class container_wrapper <boost::optional<T>>
+{ 
+	
+	public:
+	container_wrapper() = default;
+	container_wrapper(boost::optional<T> arg) : _value(std::move(arg)){}
+  container_wrapper(const container_wrapper &) = default;
+  container_wrapper(container_wrapper &&) = default;
+	container_wrapper & operator = (const container_wrapper&) = default;
+	container_wrapper & operator = (container_wrapper&&) = default;
+  std::size_t size() const
+	{ 
+		return _value?1:0;
+	};
+	//container_wrapper<std::vector<T>>(){si=s;};
+	boost::optional<T> _value;
 };
 
